c741b194b9678db98ed1195e141aed38cab0a979
Darman <71362875+darmanta@users.noreply.github.com>
2024-02-26 09:01:16 +0700

    Create fork.php

---
+++ fork.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Pengenalan Fork</title>
+  </head>
+  <body>
+    <?php
+      echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)";
+    <?
+  </body>
+</html>
