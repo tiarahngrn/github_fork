fa896e1e948499c5e86ba06e97a5156950693c0e
FriskaChristine <159239354+FriskaChristine@users.noreply.github.com>
2024-03-06 15:16:56 +0700

    Update fork.php
    Update fork.php

--- fork.php
+++ fork.php
@@ -5,6 +5,7 @@
   <body>
     <?php
       echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)";
+      echo "Friska itu orang nya setia";
     <?
   </body>
 </html>
