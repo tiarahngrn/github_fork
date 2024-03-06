165664a268b54062622c36160c4a14bb0c92c637
FriskaChristine <159239354+FriskaChristine@users.noreply.github.com>
2024-03-06 14:41:09 +0700

    Create Friska.php
    File data kampus Friska

---
+++ Friska.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Kampus</title>
+    </head>
+      <body>
+        <?php
+          echo "amik mahaputra Riau<br>";
+          echo "Alamat : Jl. Soebrantas";
+<?
+ </body>
+</html>
