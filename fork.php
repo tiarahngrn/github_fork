9810ed7222ef9d662edd50b36daee7d0921303c1
FriskaChristine <159239354+FriskaChristine@users.noreply.github.com>
2024-03-06 14:44:24 +0700

    Update fork.php
    Update Nama Friska

--- fork.php
+++ fork.php
@@ -4,7 +4,8 @@
   </head>
   <body>
     <?php
-      echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)";
+      echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)<br>";
+      echo "Friska ";
     <?
   </body>
 </html>
